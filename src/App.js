6983fc36d53c4b682c1b10462e701a1650dc6cc9
John Niyontwali <nijohn0006@gmail.com>
2022-04-02 22:22:37 +0200

    Set up Redux as the state management container and the following were added and modified: - Added redux and react-redux modules - modified the App component to be able to interact with redux store - Added a counter component - Structured redux folder structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 // imported modules
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+
 // imported components
 import Home from "./components/Home";
+
+// imported store file
+import store from "./store";
 
 /* =============================================
 App Function Component
@@ -12,15 +17,17 @@ put in order to be rendered in our root file
 
 const App = () => {
   return (
-    <div>
-      <Router>
-        <React.StrictMode>
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </React.StrictMode>
-      </Router>
-    </div>
+    <Provider store={store}>
+      <div>
+        <Router>
+          <React.StrictMode>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </React.StrictMode>
+        </Router>
+      </div>
+    </Provider>
   );
 };
 
