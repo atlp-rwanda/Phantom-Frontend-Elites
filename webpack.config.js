404588de3205ecb1008300b11883e7b8812dd0d3
David-Neza <tuyishmirend@gmail.com>
2022-04-28 14:09:58 +0200

    added serve in scripts

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const dotenv = require("dotenv");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require("webpack")
 
 dotenv.config();
 
